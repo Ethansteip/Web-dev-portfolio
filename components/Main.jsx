a8d14f94ade7f9b901f48347e69f49837e2f6a7c
Ethan <ethan.steip@gmail.com>
2023-02-08 14:24:30 -0500

    "setting up TipTap editor"

---
+++ components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Main = () => {
+
+  return (
+    <div className="w-full h-auto text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">Lets build something together</p>
+          <h1>Hi, I'm <span>Ethan</span></h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { GrLinkedinOption } from 'react-icons/ai';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
-const Navbar = () => {
+const Navbar = ({setDarkModeState}) => {
   return (
 <div className="drawer">
   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" /> 
@@ -17,9 +18,11 @@ const Navbar = () => {
           height="60"
         />
       </div>
-      <div>
-        <h3 className="lg:hidden">Ethan Steip</h3>
-      </div>
+      <label onClick={setDarkModeState} className="swap swap-rotate">
+        <input type="checkbox" />
+        <svg className="swap-on fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"/></svg>
+        <svg className="swap-off fill-current w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"/></svg>
+      </label>
       <div className="flex-none lg:hidden">
         <label htmlFor="my-drawer-3" className="btn btn-square btn-ghost">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
@@ -50,32 +53,32 @@ const Navbar = () => {
     <label htmlFor="my-drawer-3" className="drawer-overlay"></label> 
     <ul className="menu p-4 w-80 bg-base-100">
       <Link href="/">
-        <li className="btn flex items-start btn-ghost w-full">Home</li>
+        <li className="btn flex items-start btn-ghost w-5/6">Home</li>
       </Link>
       <Link href="/">
-        <li className="btn flex items-start btn-ghost w-full">About</li>
+        <li className="btn flex items-start btn-ghost w-5/6">About</li>
       </Link>
       <Link href="/">
-        <li className="btn flex items-start btn-ghost w-full">Skills</li>
+        <li className="btn flex items-start btn-ghost w-5/6">Skills</li>
       </Link>
       <Link href="/">
-        <li className="btn flex items-start btn-ghost w-full">Projects</li>
+        <li className="btn flex items-start btn-ghost w-5/6">Projects</li>
       </Link>
       <Link href="/">
-        <li className="btn flex items-start btn-ghost w-full">Contact</li>
+        <li className="btn flex items-start btn-ghost w-5/6">Contact</li>
       </Link>
 
       <div className="w-full h-auto mt-5 ml-5">
         <h3>Lets connect</h3>
-        <ul>
-          <Link href="/">
-          <GrLinkedinOption />
+        <ul className="flex justify-start gap-x-3 mt-3">
+          <Link href="/" className="btn btn-primary rounded-full">
+            <FaLinkedinIn className="" size={25}/>
           </Link>
-          <Link href="/">
-            <li className="btn flex items-start btn-ghost w-full">Contact</li>
+          <Link href="https://github.com/Ethansteip" className="btn btn-primary rounded-full">
+            <FaGithub size={25}/>
           </Link>
-          <Link href="/">
-            <li className="btn flex items-start btn-ghost w-full">Contact</li>
+          <Link href="/" className="btn btn-primary rounded-full">
+            <HiOutlineMail size={25}/>
           </Link>
         </ul>
       </div>
