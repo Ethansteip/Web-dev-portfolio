12064e9af0610b2cc3bf816b6580c88fcd3a684e
Ethan <ethan.steip@gmail.com>
2023-02-09 22:22:00 -0500

    working on fixing home page layout. Slide out drawer is affecting things.

--- components/Main.jsx
+++ components/Main.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Main = () => {
 
   return (
-    <div className="w-full h-auto text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center">
         <div>
           <p className="uppercase text-sm tracking-widest text-gray-600">Lets build something together</p>
-          <h1>Hi, I'm <span>Ethan</span></h1>
+          <h1>Hi, I'm <span className="text-primary">Ethan</span></h1>
         </div>
       </div>
     </div>
