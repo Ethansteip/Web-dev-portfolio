da638d32a5416956117886739e660ba4e5a1716c
Ethan <ethan.steip@gmail.com>
2023-03-02 14:54:09 -0500

    having issues with importing link and image next comps. update import statements

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/Link";
+import Link from "next/link";
 import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Link from 'next/Link';
-import Image from 'next/Image';
+import Link from 'next/link';
+import Image from 'next/image';
 import { FiSun } from "react-icons/fi";
 import { FaRegMoon } from 'react-icons/fa';
 
